0a5c0ffc0d0464ef3f93865f83bdbde59f3237c8
masaki_yamanaka <ki.ymnka9@gmail.com>
2020-07-26 11:28:47 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Welcome to jokes app</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome to jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
